bd8a7728074565d4e663226e2cadaec9bd74519b
ningyuuu <ningyu321@gmail.com>
2026-02-19 18:50:31 +0800

    remove nextjs logo

--- apps/timestable/app/page.tsx
+++ apps/timestable/app/page.tsx
@@ -5,17 +5,6 @@ export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="w-full max-w-6xl py-16 px-8 bg-white dark:bg-black">
-        <div className="mb-6">
-          <Image
-            className="dark:invert"
-            src="/next.svg"
-            alt="Next.js logo"
-            width={100}
-            height={20}
-            priority
-          />
-        </div>
-
         <TimestableQuiz />
       </main>
     </div>
