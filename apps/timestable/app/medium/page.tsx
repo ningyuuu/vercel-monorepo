dd94af8b9d41d1499d1ade7fa8ae95ae1ac47a40
ningyuuu <ningyu321@gmail.com>
2026-02-21 13:32:25 +0800

    reduce more padding

--- apps/timestable/app/medium/page.tsx
+++ apps/timestable/app/medium/page.tsx
@@ -3,7 +3,7 @@ import TimestableQuiz from "../../components/TimestableQuiz";
 export default function Medium() {
   return (
     <div className="flex min-h-screen items-start justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="w-full max-w-6xl py-16 px-8 bg-white dark:bg-black">
+      <main className="w-full max-w-6xl bg-white px-3 py-16 dark:bg-black sm:px-8">
         <TimestableQuiz
           firstDigitRange={{ min: 11, max: 19 }}
           secondDigitRange={{ min: 4, max: 9 }}
