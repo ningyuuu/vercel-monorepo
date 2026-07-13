8acbe47c6dead23ae672b2784733b6d7ec9062b0
ningyuuu <ningyu321@gmail.com>
2026-07-13 15:32:04 +0800

    setstate

--- apps/handmade/app/tictactoe/Board.tsx
+++ apps/handmade/app/tictactoe/Board.tsx
@@ -1,3 +1,7 @@
+"use client";
+
+import { useState } from "react";
+
 export const BLANK = 0,
   X = 1,
   O = 2;
@@ -15,26 +19,60 @@ type BoardState = [
   CellState,
 ];
 
-export function useBoardState(): BoardState {
-  return [BLANK, BLANK, BLANK, BLANK, BLANK, BLANK, BLANK, BLANK, BLANK];
+export function useBoardState(): [
+  BoardState,
+  React.Dispatch<React.SetStateAction<BoardState>>,
+] {
+  const initState: BoardState = [
+    BLANK,
+    BLANK,
+    BLANK,
+    X,
+    BLANK,
+    O,
+    BLANK,
+    BLANK,
+    BLANK,
+  ];
+  return useState<BoardState>(initState);
 }
 
-export function Board({ state }: { state: BoardState }) {
+function Cell({ state, onClick }: { state: CellState; onClick: () => void }) {
   const printStates = [" ", "X", "O"];
   return (
-    <div>
-      <p>
-        {printStates[state[0]]} | {printStates[state[1]]} |{" "}
-        {printStates[state[2]]}
-      </p>
-      <p>
-        {printStates[state[3]]} | {printStates[state[4]]} |{" "}
-        {printStates[state[5]]}
-      </p>
-      <p>
-        {printStates[state[6]]} | {printStates[state[7]]} |{" "}
-        {printStates[state[8]]}
-      </p>
+    <div
+      className="font-mono whitespace-pre border border-white px-4 py-4"
+      onClick={onClick}
+    >
+      {printStates[state]}
+    </div>
+  );
+}
+
+export function Board({
+  state,
+  updateState,
+}: {
+  state: BoardState;
+  updateState: (index: number) => void;
+}) {
+  return (
+    <div className="font-mono whitespace-pre">
+      <div className="flex ">
+        <Cell state={state[0]} onClick={() => updateState(0)} />
+        <Cell state={state[1]} onClick={() => updateState(1)} />
+        <Cell state={state[2]} onClick={() => updateState(2)} />
+      </div>
+      <div className="flex ">
+        <Cell state={state[3]} onClick={() => updateState(3)} />
+        <Cell state={state[4]} onClick={() => updateState(4)} />
+        <Cell state={state[5]} onClick={() => updateState(5)} />
+      </div>
+      <div className="flex">
+        <Cell state={state[6]} onClick={() => updateState(6)} />
+        <Cell state={state[7]} onClick={() => updateState(7)} />
+        <Cell state={state[8]} onClick={() => updateState(8)} />
+      </div>
     </div>
   );
 }
--- apps/handmade/app/tictactoe/local/page.tsx
+++ apps/handmade/app/tictactoe/local/page.tsx
@@ -1,25 +1,31 @@
+"use client";
+
 import { Card, CardContent, CardHeader, CardTitle } from "@repo/ui/card";
-import Link from "next/link";
 import { Board, useBoardState } from "../Board";
 
 export default function Home() {
-  const state = useBoardState();
+  const [state, setState] = useBoardState();
+  const updateState = (index: number) => {
+    setState((prevState) => {
+      const newState = [...prevState];
+      newState[index] = newState[index] === 0 ? 1 : 0; // Toggle between X and O
+      return newState as typeof prevState;
+    });
+  };
   return (
     <div className="min-h-screen">
       <main className="mx-auto flex w-full max-w-3xl flex-col gap-6 px-4 py-12">
-        <Link href="/tictactoe">
-          <Card>
-            <CardHeader className="items-center text-left">
-              <CardTitle className="text-3xl">Local</CardTitle>
-              <p className="text-muted-foreground">
-                Two players take turns on the same local device.
-              </p>
-            </CardHeader>
-            <CardContent className="space-y-6">
-              <Board state={state} />
-            </CardContent>
-          </Card>
-        </Link>
+        <Card>
+          <CardHeader className="items-center text-left">
+            <CardTitle className="text-3xl">Local</CardTitle>
+            <p className="text-muted-foreground">
+              Two players take turns on the same local device.
+            </p>
+          </CardHeader>
+          <CardContent className="space-y-6">
+            <Board state={state} updateState={updateState} />
+          </CardContent>
+        </Card>
       </main>
     </div>
   );
