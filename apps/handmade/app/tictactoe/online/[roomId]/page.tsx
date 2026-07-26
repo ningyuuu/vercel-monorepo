b031a89014ffd9886855b328889ee66d3c39deea
ningyuuu <ningyu321@gmail.com>
2026-07-26 13:54:04 +0800

    room state fetching

--- apps/handmade/app/tictactoe/online/[roomId]/page.tsx
+++ apps/handmade/app/tictactoe/online/[roomId]/page.tsx
@@ -1,7 +1,20 @@
 "use client";
 import { Card, CardContent, CardHeader, CardTitle } from "@repo/ui/card";
+import { RoomManager } from "../room-manager";
+import { useParams } from "next/navigation";
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
+  const router = useRouter();
+  const { roomId } = useParams();
+
+  useEffect(() => {
+    if (!roomId) {
+      router.replace("/tictactoe/online");
+    }
+  }, [roomId, router]);
+
   return (
     <div className="min-h-screen">
       <main className="mx-auto flex w-full max-w-3xl flex-col gap-6 px-4 py-12">
@@ -13,20 +26,7 @@ export default function Home() {
             </p>
           </CardHeader>
           <CardContent className="space-y-6">
-            <div className="flex flex-row gap-2">
-              <Card className="w-full">
-                <CardHeader>
-                  <CardTitle>Create a room</CardTitle>
-                </CardHeader>
-                <CardContent></CardContent>
-              </Card>
-              <Card className="w-full">
-                <CardHeader>
-                  <CardTitle>Join a room</CardTitle>
-                </CardHeader>
-                <CardContent></CardContent>
-              </Card>
-            </div>
+            <RoomManager id={roomId as string} />
           </CardContent>
         </Card>
       </main>
--- apps/handmade/app/tictactoe/online/room-manager.tsx
+++ apps/handmade/app/tictactoe/online/room-manager.tsx
@@ -1,24 +1,46 @@
 "use client";
 import { Card, CardContent, CardHeader, CardTitle } from "@repo/ui/card";
 import { Client } from "./app-client";
-import { useEffect } from "react";
+import { useEffect, useMemo, useState } from "react";
 import { FirebaseClient } from "@/app/lib/firebase";
+
+interface Props {
+  id: string;
+}
 
 // room manager allows the user to either create or join a room
 // and renders the game board when the room has 2 players.
-export function RoomManager() {
+export function RoomManager({ id }: Props) {
+  const client = useMemo(() => new FirebaseClient(), []);
+  const [roomData, setRoomData] = useState<Record<string, unknown> | null>(
+    null,
+  );
+
   useEffect(() => {
     async function fetchData() {
       try {
-        const client = new FirebaseClient();
         const firebaseData = await client.getDatabase();
         console.log("Firebase RTDB state:", firebaseData);
+
+        const room = await client.getRoom(id);
+        setRoomData(room);
       } catch (error) {
         console.error("Error fetching Firebase RTDB state:", error);
       }
     }
     fetchData();
-  }, []);
+  }, [id, client]);
+
+  async function renderRoomIfExists() {
+    try {
+      if (roomData) {
+        return <Client />;
+      }
+    } catch (error) {
+      console.error("Error fetching room data:", error);
+    }
+    return <div>Room {id} not found.</div>;
+  }
 
   return (
     <Card>
@@ -26,10 +48,7 @@ export function RoomManager() {
         <CardTitle className="text-3xl">Room Manager!</CardTitle>
         <p className="text-muted-foreground">Play against a player remotely.</p>
       </CardHeader>
-      <CardContent className="space-y-6">
-        <Client />
-      </CardContent>
+      <CardContent className="space-y-6">{renderRoomIfExists()}</CardContent>
     </Card>
   );
-  return <div></div>;
 }
